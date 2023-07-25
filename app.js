ec41fd1c8eb262473868d4a2c27ccd3c0a54bdfe
Israel Garcia Alvarez <igalvarez@gmail.com>
2023-07-25 08:56:16 +0200

    erer

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Simple\n');
+  res.send('“Per aspera ad astra\n');
 });
 
 app.listen(8080, function () {
